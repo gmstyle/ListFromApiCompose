d036d115d95e1459617ac98c013d430e276bb724
Gabriele Martina <gabriele.martina@icsone.it>
2023-11-20 09:59:32 +0100

    aggiornato compose e dipendenze

--- app/src/main/java/it/icsone/listfromapicompose/ui/states/TodoUiState.kt
+++ app/src/main/java/it/icsone/listfromapicompose/ui/states/TodoUiState.kt
@@ -5,5 +5,5 @@ import it.icsone.listfromapicompose.models.Todo
 sealed class TodoUiState {
     data class Success(val data: Todo) : TodoUiState()
     data class Error(val message: String) : TodoUiState()
-    object Loading : TodoUiState()
+    data object Loading : TodoUiState()
 }
